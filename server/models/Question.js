066e16c7b9d69e96937997c5723c668413802fcd
farhanrahman0027 <farhanrahman0027@gmail.com>
2025-08-13 22:53:56 +0530

    Add Question and User models

---
+++ server/models/Question.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const questionSchema = new mongoose.Schema({
+  questionText: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  options: [{
+    text: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    isCorrect: {
+      type: Boolean,
+      default: false
+    }
+  }],
+  category: {
+    type: String,
+    required: true,
+    default: 'General'
+  },
+  difficulty: {
+    type: String,
+    enum: ['Easy', 'Medium', 'Hard'],
+    default: 'Medium'
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Question', questionSchema);
